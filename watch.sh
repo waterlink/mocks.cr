7eadf8136d5044dfe0b3bb29b03ddab779ba2bf7
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-02 09:21:32 +0100

    port notification code in watch.sh to macosx

--- watch.sh
+++ watch.sh
@@ -2,17 +2,36 @@
 
 AUTO_COMMIT=${AUTO_COMMIT:-""}
 
+os="$(uname)"
+
 next_change() {
   x=$(cat .change-count)
   echo $((x+1)) | tee .change-count
 }
 
 watch() {
-  os="$(uname)"
   if [[ "$os" = "Linux" ]]; then
+    # to install: apt-get install inotify-tools
     inotifywait --quiet --recursive --monitor --event modify --format "%w%f" "$@"
   elif [[ "$os" = "Darwin" ]]; then
+    # to install: brew install fswatch
     fswatch -o "$@"
+  else
+    echo "Unknown OS $os"
+    exit 1
+  fi
+}
+
+notify() {
+  if [[ "$os" = "Linux" ]]; then
+    notify-send "$@"
+  elif [[ "$os" = "Darwin" ]]; then
+    # to install: brew install terminal-notifier
+    if [[ "$1" == --expire-time=* ]]; then
+      shift
+    fi
+    echo terminal-notifier -message "$@"
+    terminal-notifier -message "$@"
   else
     echo "Unknown OS $os"
     exit 1
@@ -27,11 +46,11 @@ watch src spec \
 
     if [[ $res -eq 0 ]]; then
       ! [[ -z "$AUTO_COMMIT" ]] && git add . && git commit -m "[GREEN] Change $(next_change)" && committed="(committed)"
-      notify-send --expire-time=1000 "SUCCESS $committed"
+      notify --expire-time=1000 "SUCCESS $committed"
     else
-      failure=$(cat .watch.out | grep 'Failure\|expected:\|got:')
+      failure=$(cat .watch.out | grep 'Failure\|expected:\|got:\|Error in')
       cat .watch.out
       ! [[ -z "$AUTO_COMMIT" ]] && git add . && git commit -m "[RED] Change $(next_change)" && committed="(committed)"
-      notify-send --expire-time=3000 "FAILURE:$failure $committed"
+      notify --expire-time=3000 "FAILURE:$failure $committed"
     fi
 done
