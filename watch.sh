59c672c86e55fefd2a7873f5f30a2ecf2e75a116
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-02 08:50:52 +0100

    Add watch.sh

---
+++ watch.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+AUTO_COMMIT=${AUTO_COMMIT:-""}
+
+next_change() {
+  x=$(cat .change-count)
+  echo $((x+1)) | tee .change-count
+}
+
+inotifywait --quiet --recursive --monitor --event modify --format "%w%f" src spec \
+  | while read change; do
+    crystal spec --no-color > .watch.out
+    res=$?
+    committed=
+
+    if [[ $res -eq 0 ]]; then
+      ! [[ -z "$AUTO_COMMIT" ]] && git add . && git commit -m "[GREEN] Change $(next_change)" && committed="(committed)"
+      notify-send --expire-time=1000 "SUCCESS $committed"
+    else
+      failure=$(cat .watch.out | grep 'Failure\|expected:\|got:')
+      cat .watch.out
+      ! [[ -z "$AUTO_COMMIT" ]] && git add . && git commit -m "[RED] Change $(next_change)" && committed="(committed)"
+      notify-send --expire-time=3000 "FAILURE:$failure $committed"
+    fi
+done
