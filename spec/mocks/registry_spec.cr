69e1a0f6587b4d1bd601551f320fef5ab9ed3fd4
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-06 12:51:54 +0100

    Remove meaningless Args.new all over the spec

--- spec/mocks/registry_spec.cr
+++ spec/mocks/registry_spec.cr
@@ -9,7 +9,7 @@ module Mocks
           oid = ObjectId.new(375_u64)
 
           m.received?(oid).should eq(false)
-          m.received?(oid, Args.new(["hello world"])).should eq(false)
+          m.received?(oid, ["hello world"]).should eq(false)
         end
 
         it "is true when there was such call" do
@@ -19,16 +19,16 @@ module Mocks
           m.call(oid)
           m.received?(oid).should eq(true)
 
-          m.call(oid, Args.new(["hello world"]))
-          m.received?(oid, Args.new(["hello world"])).should eq(true)
+          m.call(oid, ["hello world"])
+          m.received?(oid, ["hello world"]).should eq(true)
         end
 
         it "is false when arguments are different" do
           m = Registry.for("example class").fetch_method("say")
           oid = ObjectId.new(983_u64)
 
-          m.call(oid, Args.new(["hello test"]))
-          m.received?(oid, Args.new(["hello world"])).should eq(false)
+          m.call(oid, ["hello test"])
+          m.received?(oid, ["hello world"]).should eq(false)
         end
       end
 
@@ -47,18 +47,18 @@ module Mocks
           m.call(oid1)
           m.last_received_args(oid1).should eq(NoArgs.new)
 
-          m.call(oid1, Args.new(["hello world"]))
+          m.call(oid1, ["hello world"])
           m.call(oid1)
           m.last_received_args(oid1).should eq(NoArgs.new)
 
-          m.call(oid2, Args.new(["hello world"]))
-          m.call(oid2, Args.new(["hello test"]))
+          m.call(oid2, ["hello world"])
+          m.call(oid2, ["hello test"])
           m.last_received_args(oid1).should eq(NoArgs.new)
 
-          m.last_received_args(oid2).should eq(Args.new(["hello test"]))
+          m.last_received_args(oid2).should eq(["hello test"])
 
-          m.call(oid1, Args.new(["hello world"]))
-          m.last_received_args(oid1).should eq(Args.new(["hello world"]))
+          m.call(oid1, ["hello world"])
+          m.last_received_args(oid1).should eq(["hello world"])
         end
       end
     end
