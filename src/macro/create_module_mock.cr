7c4758889fa79215f1e2dd5ecc1bc18ddf38f4f5
Marcelo Boeira <me@marceloboeira.com>
2016-02-13 12:38:02 -0200

    Make it possible to mock module methods [#7]

---
+++ src/macro/create_module_mock.cr
@@ -0,0 +1,26 @@
+macro create_module_mock(name, &block)
+  module ::{{name.id}}
+    @@__mocks_name = "{{name.id}}"
+
+    include ::Mocks::BaseMock
+    {{block.body}}
+  end
+
+  class ::Mocks::InstanceDoubles{{name.id}} < ::Mocks::BaseDouble
+    @@name = "Mocks::InstanceDoubles{{name.id}}"
+
+    def ==(other)
+      self.same?(other)
+    end
+
+    def ==(other : Value)
+      false
+    end
+
+    macro mock(method_spec)
+      mock(\{{method_spec}}, nil, ::{{name.id}}.allocate)
+    end
+
+    {{block.body}}
+  end
+end
