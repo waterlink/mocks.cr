acecad3aea2d15019023e4b3056292d4c774a2b9
Nathan Broadbent <git@ndbroadbent.com>
2019-12-06 14:11:50 +0700

    Prevent Arithmetic overflow (OverflowError) in hash calculations by making sure addition will never overflow UInt64

--- src/mocks/registry.cr
+++ src/mocks/registry.cr
@@ -83,7 +83,7 @@ module Mocks
       end
 
       def hash
-        object_id.hash * 32 + args.hash
+        (object_id.hash >> 2) + (args.hash >> 2)
       end
 
       def inspect(io)
@@ -122,7 +122,7 @@ module Mocks
       end
 
       def hash
-        @registry_name.hash * 32 * 32 + @name.hash * 32 + @object_id.hash
+        (@registry_name.hash >> 3) + (@name.hash >> 3) + (@object_id.hash >> 3)
       end
 
       def inspect(io)
