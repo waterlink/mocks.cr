a6b32883b2ae5b39d5ff1c31ee163b3460d51ca0
Jerome Twell <jtwell1@gmail.com>
2020-04-12 20:08:42 +0100

    Compatibility fixes for 0.34.0
    * Time.new -> Time.utc
    * object_id needed to be made public, since the new hash implementation
    in 0.32 expects it to be available (presumably because of
    compare_by_identity)?
    * Used BigInt/a multi-hashing func to deal with arithmetic overflow in hash methods - I hope
    I got the spirit of what's being accomplished here correct - a bitmask'd
    unique number.
    * Changed multiplications 32*32 to bitwise shifts to make intent
    clearer.

--- spec/mocks_spec.cr
+++ spec/mocks_spec.cr
@@ -42,7 +42,7 @@ end
 
 struct StructTimeExample
   def self.now
-    Time.new(2015, 1, 10)
+    Time.utc(2015, 1, 10)
   end
 end
 
@@ -147,10 +147,10 @@ describe Mocks do
     end
 
     it "works with struct methods" do
-      StructTimeExample.now.should eq(Time.new(2015, 1, 10))
+      StructTimeExample.now.should eq(Time.utc(2015, 1, 10))
 
-      allow(StructTimeExample).to receive(self.now).and_return(Time.new(2014, 12, 22))
-      StructTimeExample.now.should eq(Time.new(2014, 12, 22))
+      allow(StructTimeExample).to receive(self.now).and_return(Time.utc(2014, 12, 22))
+      StructTimeExample.now.should eq(Time.utc(2014, 12, 22))
     end
 
     it "affects only the same class" do
